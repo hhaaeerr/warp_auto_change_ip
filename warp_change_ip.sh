0e8a5b47ff79078c43e6cd558d3f7a99ad7b711b
hhaaeerr <84891128+hhaaeerr@users.noreply.github.com>
2021-10-11 11:16:32 +0800

    Update warp_change_ip.sh

--- warp_change_ip.sh
+++ warp_change_ip.sh
@@ -11,12 +11,12 @@ do
     result=$(curl --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" 2>&1)
     if [[ "$result" == "404" ]];then
         echo -e "Originals Only, Changing IP..."
-        systemctl restart wg-quick@wgcf
+        systemctl restart wg-quick@wg
         sleep 3
 	
     elif  [[ "$result" == "403" ]];then
         echo -e "No, Changing IP..."
-        systemctl restart wg-quick@wgcf
+        systemctl restart wg-quick@wg
         sleep 3
 	
     elif  [[ "$result" == "200" ]];then
@@ -26,7 +26,7 @@ do
 		fi
         if [[ "$region" != "$area" ]];then
             echo -e "Region: ${region} Not match, Changing IP..."
-            systemctl restart wg-quick@wgcf
+            systemctl restart wg-quick@wg
             sleep 3
         else
             echo -e "Region: ${region} Done, monitoring..."
@@ -35,7 +35,7 @@ do
 
     elif  [[ "$result" == "000" ]];then
 	echo -e "Failed, retrying..."
-        systemctl restart wg-quick@wgcf
+        systemctl restart wg-quick@wg
     fi
 done
 if [[ "$input" == "n" ]]
