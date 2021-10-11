f6b2ed198f709b37a00ac196675cb510f0cbd556
hhaaeerr <84891128+hhaaeerr@users.noreply.github.com>
2021-10-11 11:18:18 +0800

    Update warp_change_ip.sh

--- warp_change_ip.sh
+++ warp_change_ip.sh
@@ -1,7 +1,6 @@
 #bin/bash!
 
-#Github @luoxue-bot
-#Blog https://ty.al
+#Caffeine
 
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
 read -r -p "Is warp installed? [y/n] " input
