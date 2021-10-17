a7781ff1df627c785247fbe3034b5e58d6560f91
hhaaeerr <84891128+hhaaeerr@users.noreply.github.com>
2021-10-17 20:42:14 +0800

    Update warp_change_ip.sh

--- warp_change_ip.sh
+++ warp_change_ip.sh
@@ -2,9 +2,7 @@
 
 
 UA_Browser="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.87 Safari/537.36"
-read -r -p "Is warp installed? [y/n] " input
 read -r -p "Input the region you want(e.g. HK,SG):" area
-while [[ "$input" == "y" ]]
 do
     result=$(curl --user-agent "${UA_Browser}" -fsL --write-out %{http_code} --output /dev/null --max-time 10 "https://www.netflix.com/title/81215567" 2>&1)
     if [[ "$result" == "404" ]];then
@@ -36,7 +34,4 @@ do
         systemctl restart wg-quick@wg
     fi
 done
-if [[ "$input" == "n" ]]
-    bash <(curl -fsSL git.io/warp.sh) 4
-    return
 fi
